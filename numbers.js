1ffb4f9a80b36fb30816f772063c1200c2d8777a
Nikita-Appfoster <nikita.kumari@appfoster.com>
2025-12-25 14:31:51 +0530

    numbers and maths

---
+++ numbers.js
@@ -0,0 +1,22 @@
+const num = 400
+const num1 = new Number(400)
+console.log(num)
+console.log(num1)
+
+
+
+/////////////maths /////////////////////
+console.log(Math)
+console.log(Math.abs(-4))
+console.log(Math.round(4.3))
+console.log(Math.ceil(4.2))
+console.log(Math.floor(4))
+
+console.log(Math.random())   // 0-1 xe beech mai aata 
+
+
+console.log(Math.floor(Math.random()*10) + 1)
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min +1)) +min)
---
+++ strings.js
@@ -0,0 +1,18 @@
+const name = "nikita"
+const reposcount = 50
+
+console.log (name + reposcount + "value ")
+console.log(`Hello my name is ${name} and my repo count is ${reposcount}`)
+
+const gamename = new String('nikita')
+console.log(gamename[0])
+console.log(gamename.__proto__)
+console.log(gamename.length)
+console.log(gamename.toUpperCase())
+console.log(gamename.charAt(2))
+
+const str1 = "            hitesh        "
+console.log(str1)
+console.log(str1.trim())
+
+
