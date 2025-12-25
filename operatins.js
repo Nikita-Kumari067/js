23d8ac583ee91a236be74d3bf6340e1f6df1031f
Nikita-Appfoster <nikita.kumari@appfoster.com>
2025-12-25 13:11:32 +0530

    summary of data type

---
+++ operatins.js
@@ -0,0 +1,30 @@
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+
+let str1 = "hELLO"
+let STR2 = "NIKITA"
+
+let str3 = str1 + STR2
+console.log(str3)
+
+console.log( 1 +"1")
+console.log("1" + 2 +2)
+console.log(1 +  2+ "3")
+
+
+const id = Symbol('123')
+const another = Symbol('124')
+console.log(id === another)
+
+const heroes = ["shaktiman", "naagraj"]
+const info ={
+    name: "nikita",
+    age: 22,
+}
+
+ const myfunction =function(){
+       console.log("nikita")
+}
+ 
