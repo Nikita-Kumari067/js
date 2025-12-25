dd81ed183e3621f57bc259b25986f72f86defd56
Nikita-Appfoster <nikita.kumari@appfoster.com>
2025-12-25 12:33:54 +0530

    data and conversion

---
+++ datatypes.js
@@ -0,0 +1,14 @@
+"use strict" // teeat all js code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+ let name = "nikita"
+ let age = 17
+ let isLoggedIn = false
+
+ // null -> standlone value 
+ // undefined = jab humne value define nhi kiya ho jaise let name 
+
+ // symbol +> unique 
+ // object 
+ console.log(typeof name);
+
--- test.js
+++ test.js
@@ -1,1 +1,7 @@
 console.log("nikita")
+var accountpassword = "12345"
+let accountcity = "jaipur"
+
+console.table([accountpassword, accountcity])
+
+// prefer not to use var beacuse of block and function scoped 
